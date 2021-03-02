22340a934fdc1d3b091aa27fc726ebedcc227979
Aman Kalaskar <78540564+amanajaykalaskar@users.noreply.github.com>
2021-03-02 15:42:41 +0530

    created a folder

---
+++ java basics/Demoif.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//if statement
+/*if else
+ * loops
+ */
+/*
+public class Demoif {
+	public static void main(String[] args) {
+		int age=18;
+		if(age>=18)
+		{
+			System.out.println("Eligible for vote");
+		}
+	}
+
+}
+*/
+
+
